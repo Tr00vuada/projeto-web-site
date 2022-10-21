ce38eca75e7f9b562c60052f260379584e94c464
Gabriel Vitorino <gabrielmiguel651@gmail.com>
2022-10-21 17:09:51 -0300

    Atualização com menu funcional(mobile e desktop)

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -9,15 +9,55 @@
 
             <img alt="Abrir Menu" 
             id="menu-button" 
-            src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg
-">
+            src="https://raw.githubusercontent.com/william-costa/wdev-mock-site-resources/master/assets/images/menu.svg"
+            v-on:click="openMenu">
+
+            <div id="menu-overlay" v-if="menuActive"  v-on:click="closeMenu">
+            </div>
+
+            <div id="menu-items" :class="{active: menuActive}">
+
+                <img alt="Antifa !" 
+                id="menu-logo" src=
+                "https://freeblmsigns.org/signs/antifa-three-arrows/antifa-three-arrows.svg">
+
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/videos">Videos</a>
+                    </li>
+                    <li>
+                        <a href="/sobre">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="/contato">Contato</a>
+                    </li>
+                </ul>
+            </div>
+
+
         </nav>
     </header>
 </template>
 
 <script>
     export default{
-        name: 'Header'
+        name: 'Header',
+        data(){
+            return{
+              menuActive: false  
+            }
+        },
+        methods:{
+           openMenu: function(){
+            this.menuActive = true
+           },
+           closeMenu: function(){
+            this.menuActive = false
+           }
+        }
 }
 </script>
 
@@ -32,7 +72,7 @@
     }
     
     #logo{
-        width: 60px;
+        width: 50px;
     }
 
     #menu-button{
@@ -43,6 +83,82 @@
     nav{
         display: flex;
         justify-content: space-between;
+        height: 60px;
+        align-items: center;
+    }
+
+    #menu-overlay{
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 40%;
+        height: 100vh;
+        background-color: #000000;
+        opacity: 0.8;
+    }
+
+    #menu-logo{
+        width: 55px;
+        margin-top: 30px;
+        margin-bottom: 10px;
+    }
+
+    #menu-items{
+        position: fixed;    
+        top: 0;
+        right: 0;
+        background-color:var(--color-background-nav);
+        width: 60%;
+        height: 100vh;
+        display: none;
+        flex-direction: column;
+        justify-content: flex-start;
+        align-items: center;
+    }
+    
+    #menu-items.active{
+       display: flex; 
+    }
+
+    ul{
+        list-style: none;
+        text-align: center;
+        height: 60px;
+        align-items: center; 
+    }
+
+    ul,
+    li{
+        margin: 20px 0px;
+    }
+
+    ul,
+    li,
+    a{
+        color: var(--color-text-light);
+
+    }
+
+    @media (min-width: 700px){
+        #menu-button,
+        #menu-logo,
+        #menu-overlay{
+            display: none;
+        }
+        #menu-items{
+            display: flex;
+            position: static;
+            height: 60px;
+            width: auto;
+        }
+        ul{
+            display: flex;
+            flex-direction: row;
+        }
+        ul li{
+            margin: 0;
+            margin-left: 10px;
+        }   
     }
 
 </style>
