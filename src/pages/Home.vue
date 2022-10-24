168c8fd01317571192ffd69cc0bd32dac4c3880d
Gabriel Vitorino <gabrielmiguel651@gmail.com>
2022-10-24 17:35:12 -0300

    Nova page adicionada

---
+++ src/pages/Home.vue
@@ -0,0 +1,25 @@
+<template>
+   <main>
+     <div class="text-top">
+        <b>Clique no olho pra dar uma espiada !</b>
+     </div>
+     <a href="https://github.com/Tr00vuada/Punk-Frog" target="blank">
+        <img alt="Confira o repositório do game !" id="Punk-frog" :src="require('file:///C:/Users/gabri/Downloads/medical-medicine-health-15-svgrepo-com.svg')">  
+     </a>  
+   </main>
+</template>
+
+<script>
+    export default{
+        name: 'Home',
+    }
+</script>
+
+<style scoped>
+    main{
+        background-color: var(--color-background-home);
+    }
+    
+
+
+</style>
